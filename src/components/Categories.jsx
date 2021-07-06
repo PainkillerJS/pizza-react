2cd0ca2c1a17db794788b5e0764167f50fadb43d
Yuriy <nikorezar@gmaiil.com>
2021-07-06 23:49:40 +0500

    Tabs ended

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Categories({ items }) {
+  const [active, setActive] = useState(null);
+
   return (
     <div className="categories">
       <ul>
-        <li className="active">Все</li>
-        {items.map((item, i) => (
-          <li onClick={() => alert(i)} key={`${item}_${i}`}>
-            {item}
-          </li>
-        ))}
+        <li onClick={() => setActive(null)} className={active === null ? 'active' : null}>
+          Все
+        </li>
+        {items &&
+          items.map((item, i) => (
+            <li
+              onClick={() => setActive(i)}
+              className={active === i ? 'active' : null}
+              key={`${item}_${i}`}>
+              {item}
+            </li>
+          ))}
       </ul>
     </div>
   );
