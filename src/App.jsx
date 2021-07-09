a5e2a22e567c039b03959e47332a1bda262cbe40
Yuriy <nikorezar@gmail.com>
2021-07-09 18:03:14 +0500

    json request part 1

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,19 @@ import { Header } from './components';
 import { Home, Cart } from './pages';
 
 function App() {
+  const [pizzas, setPizzas] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch('http://localhost:3000/db.json')
+      .then((response) => response.json())
+      .then((json) => setPizzas(json.pizzas));
+  }, []);
+
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
-        <Route exact path={'/'} component={Home} />
+        <Route exact path={'/'} render={() => <Home pizzas={pizzas} />} />
         <Route exact path={'/cart'} component={Cart} />
       </div>
     </div>
